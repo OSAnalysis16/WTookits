3570647967404a9fcafda3208c821f2299c00e78
cassyang <kinfai_yeong@outlook.com>
2016-12-22 19:50:26 +0800

    Update TF_RFFactory.java

--- src/com/conf/TF_RFFactory.java
+++ src/com/conf/TF_RFFactory.java
@@ -6,22 +6,26 @@ import com.algorithm.TermWeighting;
 /**
  * Created by cassyang on 2016/11/30.
  */
-public class TF_RFFactory implements TermWeightingFactory{
+public class TF_RFFactory  implements TermWeightingFactory{
     private static TF_RFFactory tf_rfFactory = new TF_RFFactory();
-    private TF_RFFactory (){}
+    private TF_RFFactory(){}
 
-    public static TF_RFFactory  getIF_RFFactory()
-    {
+    public static TF_RFFactory getTf_RfFactory(){
         return tf_rfFactory;
     }
 
     @Override
     public String getName() {
-        return "IF_RF";
+        return "TF-RF";
     }
 
     @Override
     public TermWeighting getTermWeightingInstance() {
         return new TF_RF();
+    }
+
+    @Override
+    public Boolean isSupervised() {
+        return true;
     }
 }
